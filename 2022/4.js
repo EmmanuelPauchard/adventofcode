e31bce75b018f9826243ad5852e753999e3a57dc
Emmanuel Pauchard <emmanuel.pauchard@gmail.com>
2022-12-07 21:22:33 +0100

    Day 4

---
+++ 2022/4.js
@@ -0,0 +1,25 @@
+
+fs = require('fs');
+const regex = /(\d+)-(\d+),(\d+)-(\d+)/g;
+
+function is_superset(a1, a2, b1, b2) {
+    return (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2)
+}
+
+function does_overlap(a1, a2, b1, b2) {
+    return (a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2) || is_superset(a1, a2, b1, b2)
+}
+
+fs.readFile('/mnt/c/epauchard/projects/adventofcode/2022/input_4.txt', 'utf8', function (err,data) {
+    if (err) {
+      return console.log(err);
+    }
+    const array_of_matches = [...data.matchAll(regex)];
+
+    let supersets = array_of_matches.filter(x => is_superset(parseInt(x[1]), parseInt(x[2]), parseInt(x[3]), parseInt(x[4])) === true, array_of_matches);
+    let overlaps = array_of_matches.filter(x => does_overlap(parseInt(x[1]), parseInt(x[2]), parseInt(x[3]), parseInt(x[4])) === true, array_of_matches);
+
+    // console.log(array_of_matches)
+    console.log(`Number of pairs where one set is included in another: ${supersets.length}`)
+    console.log(`Number of pairs where one set overlaps another: ${overlaps.length}`)
+  });
