793099a39409037711c6227767e5264e8721bab8
Emmanuel Pauchard <emmanuel.pauchard@gmail.com>
2022-12-21 21:02:56 +0100

    Day 1: javascript file cleanup

--- 2022/01.js
+++ 2022/01.js
@@ -1,10 +1,8 @@
-console.log("Hello")
 fs = require('fs');
-fs.readFile('/mnt/c/epauchard/projects/adventofcode/2022/1/input.txt', 'utf8', function (err,data) {
+fs.readFile('2022/1.in', 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
-    // console.log(data);
     let elements = data.split('\n\n');
     elements = elements.map(e => e.split("\n").map(x => parseInt(x)));
     elements = elements.map(x => x.reduce((partialSum, a) => partialSum + a, 0));
