0ecf2317ce327f49b01f2b935d537c63e908e5e6
Emmanuel Pauchard <emmanuel.pauchard@gmail.com>
2022-12-22 13:31:12 +0100

    Day 1 and 4 Javascript challenges: improve style and documentation
    Include test data so that the script can be tested with node directly.

--- 2022/01.js
+++ 2022/01.js
@@ -1,12 +1,48 @@
-fs = require('fs');
-fs.readFile('2022/1.in', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    let elements = data.split('\n\n');
-    elements = elements.map(e => e.split("\n").map(x => parseInt(x)));
-    elements = elements.map(x => x.reduce((partialSum, a) => partialSum + a, 0));
-    elements = elements.sort((a, b) => b - a);
+/**
+ * Advent Of Code Day 1
+ * Javascript implementation
+ * Sum and sort integers lists
+ */
+const test_data = `
+1000
+2000
+3000
 
-    console.log(elements.slice(0, 3))
-  });
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+`;
+
+fs = require('fs');
+
+/**
+ * Solve Day 1 challenge. Solution is sent to stdlog
+ * @param {string} data The challenge's input data
+ */
+function solve(data) {
+  let elements = data.trim().split('\n\n');  // Split group of values
+  elements = elements.map(e => e.split("\n").map(x => parseInt(x)));  // For each group, convert to integer
+  elements = elements.map(x => x.reduce((acc, a) => acc + a, 0));  // Sum over each group
+  elements = elements.sort((a, b) => b - a);  // Sort each group
+
+  // Part 1 needs to get the maximum, so first index of the list
+  console.log("Part 1: maximum is: ", elements[0])
+  // Part 2 needs to sum all top 3
+  console.log("Part 2: sum of top 3: ", elements.slice(0, 3).reduce((acc, a) => acc + a, 0))
+}
+
+fs.readFile('1.in', 'utf8', function (err, data) {
+  if (err) {
+    console.log(`Error ${err}: fallback using test data`)
+    solve(test_data);
+  } else {
+    solve(data);
+  }
+});
--- 2022/04.js
+++ 2022/04.js
@@ -1,24 +1,63 @@
+/**
+ * Challenge Day 4: overlapping sets of integers
+ */
+
+const test_data = `
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+`;
 
 fs = require('fs');
-const regex = /(\d+)-(\d+),(\d+)-(\d+)/g;
 
+/**
+ * Check if range [a1,a2] is a superset (ie: a set that totally includes) range [b1,b2]
+ * @param {int} a1 Lower bound of the first range
+ * @param {int} a2 Upper bound of the first range
+ * @param {int} b1 Lower bound of the second range
+ * @param {int} b2 Upper bound of the second range
+ * @returns {boolean} True if interval a is a superset of interval b
+ */
 function is_superset(a1, a2, b1, b2) {
-    return (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2)
+  // Return true
+  return (a1 >= b1 && a2 <= b2) || (a1 <= b1 && a2 >= b2)
 }
 
+/**
+ * Check if range [a1,a2] overlaps (ie: has a non empty intersection with) range [b1,b2]
+ * @param {int} a1 Lower bound of the first range
+ * @param {int} a2 Upper bound of the first range
+ * @param {int} b1 Lower bound of the second range
+ * @param {int} b2 Upper bound of the second range
+ * @returns {boolean} True if interval a overlaps interval b
+ */
 function does_overlap(a1, a2, b1, b2) {
-    return (a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2) || is_superset(a1, a2, b1, b2)
+  return (a1 >= b1 && a1 <= b2) || (a2 >= b1 && a2 <= b2) || is_superset(a1, a2, b1, b2)
 }
 
-fs.readFile('4.in', 'utf8', function (err,data) {
-    if (err) {
-      return console.log(err);
-    }
-    const array_of_matches = [...data.matchAll(regex)];
+/**
+ * Solve Day 4 challenge. Solution is sent to stdlog
+ * @param {string} data The challenge's input data
+ */
+function solve(data) {
+  const regex = /(\d+)-(\d+),(\d+)-(\d+)/g;
+  const array_of_matches = [...data.matchAll(regex)];
 
-    let supersets = array_of_matches.filter(x => is_superset(parseInt(x[1]), parseInt(x[2]), parseInt(x[3]), parseInt(x[4])) === true, array_of_matches);
-    let overlaps = array_of_matches.filter(x => does_overlap(parseInt(x[1]), parseInt(x[2]), parseInt(x[3]), parseInt(x[4])) === true, array_of_matches);
+  let supersets = array_of_matches.filter(x => is_superset(parseInt(x[1]), parseInt(x[2]), parseInt(x[3]), parseInt(x[4])) === true, array_of_matches);
+  let overlaps = array_of_matches.filter(x => does_overlap(parseInt(x[1]), parseInt(x[2]), parseInt(x[3]), parseInt(x[4])) === true, array_of_matches);
 
-    console.log(`Number of pairs where one set is included in another: ${supersets.length}`)
-    console.log(`Number of pairs where one set overlaps another: ${overlaps.length}`)
-  });
+  console.log(`Number of pairs where one set is included in another: ${supersets.length}`)
+  console.log(`Number of pairs where one set overlaps another: ${overlaps.length}`)
+}
+
+fs.readFile('4.in', 'utf8', function (err, data) {
+  if (err) {
+    console.log(`Error ${err}: fallback using test data`)
+    solve(test_data);
+  } else {
+    solve(data);
+  }
+});
